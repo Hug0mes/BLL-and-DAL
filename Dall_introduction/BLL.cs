fbd619dd39a35b11c09e6bcf17007a4aecee627e
Hugo Gomes <79423600+Hug0mes@users.noreply.github.com>
2021-12-09 10:08:53 +0000

    "0.1"

---
+++ Dall_introduction/BLL.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccessLayer;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLogicLayer
+{
+    public class BLL
+    {
+        public class Imagem
+        {
+            static public object loadpic()
+            {
+                DAL dal = new DAL();
+                 SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", 1),
+             };
+                return dal.executarScalar("select Img from Imagem where id=1", sqlParams);
+            
+            }
+            static public DataTable Load()
+            {
+                DAL dal = new DAL();
+                return dal.executarReader("select * from Imagem", null);
+            }
+
+            static public int insertImagem(byte [] img )
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@img", img),
+                
+           };
+
+                return dal.executarNonQuery("INSERT into Imagem (Img) VALUES(@img)", sqlParams);
+            }
+        }
+        public class Utilizadores
+        {
+
+            static public DataTable Load()
+            {
+                DAL dal = new DAL();
+                return dal.executarReader("select * from Utilizadores", null);
+            }
+            static public int insertUtilizadores(string Nome, string Email, string Telefone)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@Nome", Nome),
+                new SqlParameter("@Email", Email),
+                new SqlParameter("@Telefone", Telefone)
+           };
+
+                return dal.executarNonQuery("INSERT into Utilizadores (Nome,Email,Telefone) VALUES(@Nome,@Email,@Telefone)", sqlParams);
+            }
+            static public DataTable queryCliente_Like(String nome)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@nome", nome + "%")
+                };
+                return dal.executarReader("select * from Clientes where Nome like @nome", sqlParams);
+            }
+            static public DataTable queryCliente(int id) {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id)
+                };
+                return dal.executarReader("select * from Clientes where ID=@id", sqlParams);
+            }
+            static public DataTable queryCliente_2(int id, string nome)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id),
+                 new SqlParameter("@Nome", nome)
+                };
+                return dal.executarReader("select * from Clientes where ID=@id and Nome=@nome", sqlParams);
+            }
+            static public DataTable queryCliente_3(int id)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id)
+                };
+                return dal.executarReader("select * from Clientes where ID=@id", sqlParams);
+            }
+            static public int updateCliente(string id, string nome, string morada, string telefone)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id),
+                new SqlParameter("@nome", nome),
+                new SqlParameter("@morada", morada),
+                new SqlParameter("@telefone", telefone)
+            };
+                return dal.executarNonQuery("update [Clientes] set [nome]=@nome, [morada]=@morada, [telefone]=@telefone where [id]=@id", sqlParams);
+            }
+            
+            static public int deleteUtilizadores(int id)            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlParams = new SqlParameter[]{
+                new SqlParameter("@id", id),
+               
+            };
+                return dal.executarNonQuery("Delete From Utilizadores WHERE[id]=@id", sqlParams);
+            }
+
+                static public int alterarPerfil(string utilizador, String password, string imagem)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlparams = new SqlParameter[]{
+                    new SqlParameter("@utilizador", utilizador),
+                    new SqlParameter("@password", password),
+                    new SqlParameter("@imagem", imagem)};
+
+                return dal.executarNonQuery("update [utilizadores] set [password]=@password, [imagem]=@imagem where [utilizador]=@utilizador", sqlparams);
+            }
+
+            static public int alterarEstado(string utilizador, int estado)
+            {
+                DAL dal = new DAL();
+                SqlParameter[] sqlparams = new SqlParameter[]{
+                    new SqlParameter("@utilizador", utilizador),
+                    new SqlParameter("@estado", estado)};
+
+                return dal.executarNonQuery("update utilizadores set estado=@estado where utilizador=@utilizador", sqlparams);
+            }
+
+        }
+    }
+}
---
+++ Dall_introduction/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogicLayer;
+namespace Dall_introduction
+{
+    public partial class Formm : Form
+    {
+       int id = 0;
+        public Formm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                int x = BLL.Utilizadores.insertUtilizadores(textBox1.Text, textBox2.Text, textBox3.Text);
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+              
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Erro:" + ex, "Dados Não Inseridos"); 
+            }
+           
+            
+        }
+
+        private void Formm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = BLL.Utilizadores.Load();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Tens certeza madje??", "?", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                int x = BLL.Utilizadores.deleteUtilizadores(id);
+                dataGridView1.DataSource = BLL.Utilizadores.Load();
+
+            }
+        }
+    }
+}
